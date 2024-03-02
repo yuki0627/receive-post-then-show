a97a72da122e3cb17494c0c6a44cbdce66587652
Yuki Takahashi <yuki0627@gmail.com>
2024-03-03 06:54:48 +0900

    データを先頭にいれる

--- app.py
+++ app.py
@@ -11,15 +11,12 @@ def test_endpoint():
     # リクエストからパラメータを取得
     # data = request.form.to_dict()  # formデータの場合
     data = request.json  # JSONデータの場合
-    received_data_list.append(data)
-    print(data)
-    print(received_data_list)
+    received_data_list.insert(0, data)
     return "パラメータを受け取りました。"
 
 @app.route('/')
 def display_data():
     global received_data_list
-    print(received_data_list)
     return render_template("index.html", received_data_list=received_data_list)
 
 if __name__ == '__main__':
