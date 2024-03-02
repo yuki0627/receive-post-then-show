72852e82c02a7ecbc4c50a4cb45cd74addb88826
Yuki Takahashi <yuki0627@gmail.com>
2024-03-03 06:45:41 +0900

    htmlを外出し

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string,render_template
 
 app = Flask(__name__)
 
@@ -18,21 +18,9 @@ def test_endpoint():
 
 @app.route('/')
 def display_data():
-    # メモリに保存されたデータを表示
     global received_data
     print(received_data)
-    return render_template_string("""
-        <h1>受け取ったデータ</h1>
-        {% if data %}
-            <ul>
-            {% for key, value in data.items() %}
-                <li>{{ key }}: {{ value }}</li>
-            {% endfor %}
-            </ul>
-        {% else %}
-            <p>データがありません。</p>
-        {% endif %}
-    """, data=received_data)
+    return render_template("index.html", data=received_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
